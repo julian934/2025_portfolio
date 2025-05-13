b49da803e99284471ebe5dddce729b0588b35472
Julian Thatguy <jborner111@gmail.com>
2025-05-13 14:41:59 -0500

    test build 3.0

--- src/app/utils/lib/utils.ts
+++ src/app/utils/lib/utils.ts
@@ -1,6 +1,6 @@
 import { ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
-import type { Metadata } from 'next'
+
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
